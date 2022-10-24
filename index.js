db4c8247d471394643ba696f0c21e6bf8f40236c
qwe8851 <dgh07027@naver.com>
2022-10-25 00:02:03 +0900

    literal types & as const 문법 연습

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-// 타입변수 & readonly
-var 동물 = { name: 'kim', age: 20 };
-// readonly 연습
-var 출생지역 = { region: 'seoul' };
-출생지역.region = 'busan'; // const변수는 재할당만 불가, 수정은 가능'
-var 여친 = { name: '엠버' };
-여친.name = '유라';
-var position = { x: 10, y: 20 };
-var test = {
-    name: 'kim',
-    phone: 12341234,
-    email: 'qwe@naver.com',
-    adult: true
+// literal types와 const문법
+// literal type - 더 엄격한 관리 가능 
+var 누구세요;
+누구세요 = '대머리';
+function 함수(a) {
+    return 0;
+}
+함수('hello');
+function 가위바위보(a) {
+    return ['가위'];
+}
+가위바위보('가위');
+// const 변수
+var 자료 = {
+    name: 'kim'
+}; // 자료.name을 콘솔창에 뽑아보면 kim이 나옴
+function 내함수(a) {
+    //"kim"이라는 자료만 들어올 수 있습니다 X
+    //"kim"이라는 타입만 들어올 수 있습니다 O
+}
+내함수(자료.name); //그래서 이렇게 하면 string타입이 들어오므로 에러가 남 
+// const - 솔루션 1
+var 자료2 = {
+    name: 'kim'
 };
+function 내함수2(a) {
+}
+내함수2(자료2.name); // 에러가 나지 않음
+// const - 솔루션 2(as const)
+var 자료4 = {
+    name: 'kim'
+};
+function 내함수4(a) {
+}
+내함수2(자료4.name);
+// as const
+// 1. 타입을 object의 value로 바꿔줌 ('kim'으로 타입 변경)
+// 2. object안에 있는 모든 속성을 readonly로 변경 (변경하면 에러 발생)
--- index.ts
+++ index.ts
@@ -1,69 +1,53 @@
-// 타입변수 & readonly
+// literal types와 const문법
 
-// type alias 연습
-type Animal = {name:string, age:number}
-let 동물 :Animal = {name : 'kim', age : 20}
+// literal type - 더 엄격한 관리 가능 
+let 누구세요 :'대머리' | '솔로';
+누구세요 = '대머리';
 
-
-// readonly 연습
-const 출생지역 = {region : 'seoul'}
-출생지역.region = 'busan' // const변수는 재할당만 불가, 수정은 가능'
-
-
-type Girlfriend = {
-    readonly name : string
+function 함수(a:'hello') :1|0{
+    return 0;
 }
-const 여친 :Girlfriend = { name : '엠버'}
-여친.name = '유라' 
+함수('hello');
 
-// type 변수 union type으로 합치기(extend) 가능
-type Name = string;
-type Age = number;
-type Person = Name | Age;
-
-type PositionX = { x:number };
-type PositionY = { y:number };
-type NewType = PositionX&PositionY; // {x:number, y:number}
-let position :NewType = {x:10, y:20};
-
-
-// 숙제1
-type type1_1 = string;
-type type1_2 = number | string;
-type newType1 = Name | Age;
-// let test:newType1 = 'string'
-
-// 숙제2
-type newType2 = {
-    color? :string,
-    size : number,
-    readonly position : number[]
+function 가위바위보(a:'가위'|'바위'|'보') :('가위'|'바위'|'보')[]{
+    return ['가위'];
 }
-// let test:newType2 = {
-//     size : 123,
-//     position : [1,2,3]
-// }
+가위바위보('가위');
 
-// 숙제3
-type newType3 = {
-    name : string,
-    phone : number,
-    email : string
-}
-// let test:newType3 = {
-//     name : 'kim',
-//     phone : 12341234,
-//     email : 'qwe@naver.com'
-// }
 
-// 숙제4
-type newType4 = {
-    adult : boolean
-}
+// const 변수
+let 자료 ={
+    name : 'kim'
+} // 자료.name을 콘솔창에 뽑아보면 kim이 나옴
 
-let test:newType3 & newType4  = {
-    name : 'kim',
-    phone : 12341234,
-    email : 'qwe@naver.com',
-    adult : true
+function 내함수(a:'kim'){ // 파라미터 타입에 "타입이 kim만 들어올 수 있다"라고 지정해준 것
+    //"kim"이라는 자료만 들어올 수 있습니다 X
+    //"kim"이라는 타입만 들어올 수 있습니다 O
 }
+내함수(자료.name);  //그래서 이렇게 하면 string타입이 들어오므로 에러가 남 
+
+
+// const - 솔루션 1
+let 자료2 :{name:'kim'} = { // 애초에 타입을 kim으로 지정해주면
+    name : 'kim'
+}
+function 내함수2(a:'kim'){
+
+}
+내함수2(자료2.name) // 에러가 나지 않음
+
+
+// const - 솔루션 2(as const)
+let 자료4 = { 
+    name : 'kim'
+} as const
+
+function 내함수4(a:'kim'){
+
+}
+내함수2(자료4.name)
+
+
+// as const
+// 1. 타입을 object의 value로 바꿔줌 ('kim'으로 타입 변경)
+// 2. object안에 있는 모든 속성을 readonly로 변경 (변경하면 에러 발생)
