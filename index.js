521dc32495676e96e9eea9d5318ba92c0c9d3374
qwe8851 <dgh07027@naver.com>
2022-10-24 23:13:39 +0900

    타입 alias & readonly 연습

--- index.js
+++ index.js
@@ -1,55 +1,14 @@
-// 타입확정하기 Narrowing & Assertion
-// Narrowing - 타입이 아직 하나로 확정되지 않았을경우
-// typeof 변수
-// 속성명 in 오브젝트자료
-// 인스턴스 instanceof 부모
-function 함수(x) {
-    if (typeof x === 'string')
-        return x + 'string type';
-    if (typeof x === 'number')
-        return x + 'number type';
-    else
-        return 0; // else를 안넣으면 에러를 내주는데 이게 싫으면 tsconfig.json에 "noImplicitReturns" : false를 추가하면 됨
-}
-function 함수2(x) {
-    var arr = [];
-    if (typeof x === 'number')
-        arr[0] = x;
-}
-// Assertion
-// 1. narrowing할 대 사용 (union타입 중 하나로 확정할 때 )
-// 2. 무슨 타입이 들어올지 100%확실할 때
-// 걍 안쓰는게 좋음
-function 함수3(x) {
-    var arr = [];
-    arr[0] = x; // 타입덮어쓰기
-}
-function 변환기(data) {
-    return JSON.parse(data);
-}
-var jake = 변환기('{"name" : "kim"}');
-// 숙제 1 - 클리닝 함수
-function 클리닝함수(a) {
-    var 클리닝완료 = [];
-    a.forEach(function (b) {
-        if (typeof b === 'string')
-            클리닝완료.push(parseFloat(b));
-        else
-            클리닝완료.push(b);
-    });
-    return 클리닝완료;
-}
-// console.log(클리닝함수([123,'3']))
-// 숙제 2 - 함수만들기
-var 철수쌤 = { subject: 'math' };
-var 영희쌤 = { subject: ['science', 'english'] };
-var 민수쌤 = { subject: ['science', 'art', 'korean'] };
-function arr검사(a) {
-    if (Array.isArray(a.subject))
-        return a.subject[a.subject.length - 1];
-    else if (typeof a.subject == 'string')
-        return a.subject;
-    else
-        return '없쪄';
-}
-console.log(arr검사(민수쌤));
+// 타입변수 & readonly
+var 동물 = { name: 'kim', age: 20 };
+// readonly 연습
+var 출생지역 = { region: 'seoul' };
+출생지역.region = 'busan'; // const변수는 재할당만 불가, 수정은 가능'
+var 여친 = { name: '엠버' };
+여친.name = '유라';
+var position = { x: 10, y: 20 };
+var test = {
+    name: 'kim',
+    phone: 12341234,
+    email: 'qwe@naver.com',
+    adult: true
+};
--- index.ts
+++ index.ts
@@ -1,72 +1,69 @@
-// 타입확정하기 Narrowing & Assertion
+// 타입변수 & readonly
 
-// Narrowing - 타입이 아직 하나로 확정되지 않았을경우
-// typeof 변수
-// 속성명 in 오브젝트자료
-// 인스턴스 instanceof 부모
-function 함수 (x:number|string){
-    if(typeof x === 'string') return x+'string type'
-    if(typeof x === 'number') return x+'number type'
-    else return 0; // else를 안넣으면 에러를 내주는데 이게 싫으면 tsconfig.json에 "noImplicitReturns" : false를 추가하면 됨
+// type alias 연습
+type Animal = {name:string, age:number}
+let 동물 :Animal = {name : 'kim', age : 20}
+
+
+// readonly 연습
+const 출생지역 = {region : 'seoul'}
+출생지역.region = 'busan' // const변수는 재할당만 불가, 수정은 가능'
+
+
+type Girlfriend = {
+    readonly name : string
+}
+const 여친 :Girlfriend = { name : '엠버'}
+여친.name = '유라' 
+
+// type 변수 union type으로 합치기(extend) 가능
+type Name = string;
+type Age = number;
+type Person = Name | Age;
+
+type PositionX = { x:number };
+type PositionY = { y:number };
+type NewType = PositionX&PositionY; // {x:number, y:number}
+let position :NewType = {x:10, y:20};
+
+
+// 숙제1
+type type1_1 = string;
+type type1_2 = number | string;
+type newType1 = Name | Age;
+// let test:newType1 = 'string'
+
+// 숙제2
+type newType2 = {
+    color? :string,
+    size : number,
+    readonly position : number[]
+}
+// let test:newType2 = {
+//     size : 123,
+//     position : [1,2,3]
+// }
+
+// 숙제3
+type newType3 = {
+    name : string,
+    phone : number,
+    email : string
+}
+// let test:newType3 = {
+//     name : 'kim',
+//     phone : 12341234,
+//     email : 'qwe@naver.com'
+// }
+
+// 숙제4
+type newType4 = {
+    adult : boolean
 }
 
-function 함수2 (x:number|string){
-    let arr :number[] = [];
-
-    if(typeof x === 'number') arr[0] = x
-    
+let test:newType3 & newType4  = {
+    name : 'kim',
+    phone : 12341234,
+    email : 'qwe@naver.com',
+    adult : true
 }
-
-
-// Assertion
-// 1. narrowing할 대 사용 (union타입 중 하나로 확정할 때 )
-// 2. 무슨 타입이 들어올지 100%확실할 때
-// 걍 안쓰는게 좋음
-function 함수3 (x:number|string){
-    let arr :number[] = [];
-
-    arr[0] = x as number; // 타입덮어쓰기
-}
-
-type Person ={
-    name : string
-}
-
-function 변환기<T>(data:string):T{
-    return JSON.parse(data) as T;
-} 
-const jake = 변환기<Person>('{"name" : "kim"}');
-
-
-
-
-
-// 숙제 1 - 클리닝 함수
- function 클리닝함수 (a:(string |number)[]){
-    let 클리닝완료 : number[] = [];
-
-    a.forEach((b) => {
-        if(typeof b === 'string') 클리닝완료.push(parseFloat(b))
-        else 클리닝완료.push(b)
-    });
-    
-    return 클리닝완료
- }
-// console.log(클리닝함수([123,'3']))
-
-
-
-// 숙제 2 - 함수만들기
-let 철수쌤 = { subject : 'math' }
-let 영희쌤 = { subject : ['science', 'english'] }
-let 민수쌤 = { subject : ['science', 'art', 'korean'] }
-
-function arr검사 (a : {subject : string | string[]} ){
-    if(Array.isArray(a.subject)) 
-        return a.subject[a.subject.length -1]
-    else if (typeof a.subject == 'string')
-        return a.subject
-    else return '없쪄'
-}
-
-console.log(arr검사(민수쌤))
