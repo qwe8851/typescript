6dc089a77a748d89f66e1b915accf985038629fb
qwe8851 <dgh07027@naver.com>
2022-10-25 23:06:05 +0900

    함수 method아 type alias 지정 연습

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-// literal types와 const문법
-// literal type - 더 엄격한 관리 가능 
-var 누구세요;
-누구세요 = '대머리';
-function 함수(a) {
-    return 0;
-}
-함수('hello');
-function 가위바위보(a) {
-    return ['가위'];
-}
-가위바위보('가위');
-// const 변수
-var 자료 = {
-    name: 'kim'
-}; // 자료.name을 콘솔창에 뽑아보면 kim이 나옴
-function 내함수(a) {
-    //"kim"이라는 자료만 들어올 수 있습니다 X
-    //"kim"이라는 타입만 들어올 수 있습니다 O
-}
-내함수(자료.name); //그래서 이렇게 하면 string타입이 들어오므로 에러가 남 
-// const - 솔루션 1
-var 자료2 = {
-    name: 'kim'
+// 함수 methods에 type alias 지정하는 방법
+var 함수 = function () {
+    return 10;
 };
-function 내함수2(a) {
-}
-내함수2(자료2.name); // 에러가 나지 않음
-// const - 솔루션 2(as const)
-var 자료4 = {
-    name: 'kim'
+var ABC = function (x, y) { return x + y; };
+var 회원정보 = {
+    name: 'kim',
+    age: 20,
+    plusOne: function (x) {
+        return x + 1;
+    },
+    changeName: function () {
+        console.log('안녕');
+    }
 };
-function 내함수4(a) {
+회원정보.plusOne(1);
+회원정보.changeName();
+var cutZero = function (x) {
+    var result = x.replace(/^0+/, "");
+    return result;
+};
+function removeDash(x) {
+    var result = x.replace(/-/g, "");
+    return parseFloat(result);
 }
-내함수2(자료4.name);
-// as const
-// 1. 타입을 object의 value로 바꿔줌 ('kim'으로 타입 변경)
-// 2. object안에 있는 모든 속성을 readonly로 변경 (변경하면 에러 발생)
+// 숙제 3 - 어려움 ㅜ 보고풀었음
+// 타입없이 먼저 작성
+function func3(x, func1, func2) {
+    var result = func1(x); // string타입 x라는 파라미터를 func1함수에 삽입, 결과를 result에 저장
+    var result2 = func2(result); // 윗줄의 결과를 func2에 삽입
+    console.log(result2); // 최종 결과를 콘솔창에 출력
+}
+func3('010-0000-0000', cutZero, removeDash);
+function func4(x, func1, func2) {
+    var result = func1(x);
+    var result2 = func2(result);
+    console.log(result2);
+}
+func3('010-0000-0000', cutZero, removeDash);
--- index.ts
+++ index.ts
@@ -1,53 +1,69 @@
-// literal types와 const문법
+// 함수 methods에 type alias 지정하는 방법
 
-// literal type - 더 엄격한 관리 가능 
-let 누구세요 :'대머리' | '솔로';
-누구세요 = '대머리';
+//함수 표현식을 사용해서 함수타입 지정
+// 연습1
+type 함수타입 = (a:string) => number;
 
-function 함수(a:'hello') :1|0{
-    return 0;
+let 함수:함수타입 = function(){
+    return 10;
 }
-함수('hello');
 
-function 가위바위보(a:'가위'|'바위'|'보') :('가위'|'바위'|'보')[]{
-    return ['가위'];
+//연습 2
+type NumOut = (x:number, y:number) => number;
+let ABC:NumOut = function(x,y){return x+y}
+
+
+//methods 안에 타입 지정
+//숙제 1
+type Member = {
+    name: string,
+    age : number,
+    plusOne : (x:number) => number,
+    changeName : () => void
 }
-가위바위보('가위');
-
-
-// const 변수
-let 자료 ={
-    name : 'kim'
-} // 자료.name을 콘솔창에 뽑아보면 kim이 나옴
-
-function 내함수(a:'kim'){ // 파라미터 타입에 "타입이 kim만 들어올 수 있다"라고 지정해준 것
-    //"kim"이라는 자료만 들어올 수 있습니다 X
-    //"kim"이라는 타입만 들어올 수 있습니다 O
+let 회원정보 : Member = {
+    name : 'kim',
+    age : 20,
+    plusOne(x){
+        return x +1
+    },
+    changeName : ()=>{
+        console.log('안녕');
+    }
 }
-내함수(자료.name);  //그래서 이렇게 하면 string타입이 들어오므로 에러가 남 
+회원정보.plusOne(1)
+회원정보.changeName();
 
 
-// const - 솔루션 1
-let 자료2 :{name:'kim'} = { // 애초에 타입을 kim으로 지정해주면
-    name : 'kim'
+//숙제 2
+type CutType = (x:string) => string;
+
+let cutZero:CutType = function(x){
+    let result = x.replace(/^0+/, "");
+    return result;
 }
-function 내함수2(a:'kim'){
 
+function removeDash(x:string) : number{
+    let result = x.replace(/-/g, "");
+    return parseFloat(result);
 }
-내함수2(자료2.name) // 에러가 나지 않음
 
-
-// const - 솔루션 2(as const)
-let 자료4 = { 
-    name : 'kim'
-} as const
-
-function 내함수4(a:'kim'){
-
+// 숙제 3 - 어려움 ㅜ 보고풀었음
+// 타입없이 먼저 작성
+function func3(x, func1, func2){
+    let result = func1(x);    // string타입 x라는 파라미터를 func1함수에 삽입, 결과를 result에 저장
+    let result2 = func2(result);    // 윗줄의 결과를 func2에 삽입
+    console.log(result2);   // 최종 결과를 콘솔창에 출력
 }
-내함수2(자료4.name)
+func3('010-0000-0000', cutZero, removeDash);
 
+// 타입지정(최종코드)
+type type1 = (x:string) => string;  
+type type2 = (x:string) => number; // 데쉬를 없앳으니 문자만 남음
 
-// as const
-// 1. 타입을 object의 value로 바꿔줌 ('kim'으로 타입 변경)
-// 2. object안에 있는 모든 속성을 readonly로 변경 (변경하면 에러 발생)
+function func4(x:string, func1:type1, func2:type1){
+    let result = func1(x);
+    let result2 = func2(result);   
+    console.log(result2);   
+}
+func3('010-0000-0000', cutZero, removeDash);
