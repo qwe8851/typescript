a570b641d3f93542972c169ac9b56840e84f40a8
qwe8851 <dgh07027@naver.com>
2022-10-30 15:51:07 +0900

    함수에 restparameter사용해보기 & destructuring 타입 지정 연습해보기

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
-// Object 타입지정(interface) 연습해보기
-var 네모 = { color: 'red', width: 100 };
-var 학생 = { name: "kim" };
-var 선생 = { name: "kim", age: 20 };
-var 고양이 = { name: "나비", age: 3 };
-// type VS interface
-// - interface는 중복선언 가능
-// - type은 중복선언 불가능
+// 함수 rest parameter, destructuring할 때 타입지정
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// 1. rest parameter
+function 함수() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    console.log(a);
+}
+함수(1, 2, 3, 4, 5, 6);
+// 1-2. ...spread operation
+// array나 object왼쪽에 쓰면 괄호를 벗겨달라는 문법임.
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5];
+var arr3 = __spreadArray(__spreadArray([], arr1, true), arr2, true); // console 출력결과 : 1,2,3,4,5
+console.log(arr3);
+// 2. destructuring 
+// 2-1. destructuring - arr
+var _a = ["안녕", 100], 변수1 = _a[0], 변수2 = _a[1];
+console.log(변수1);
+console.log(변수2);
+// 2-2.destructuring - obj
+var _b = { student: true, age: 20 }, student = _b.student, age = _b.age; //변수명과 속성명을 맞추는게 편리(헷갈릴 수 있음)
+var _c = { student1: true, age1: 20 }, student1 = _c.student1, age1 = _c.age1; // 변수명과 속성명이 동일하면 생략이 가능함
+console.log(student1);
+console.log(age1);
+// 3. 함수에도 destructuring문법 사용 가능
+// ex. obj안에 있던 자료를 파라미터로 집어넣고 싶다면?
+var 오브젝트 = { student2: true, age2: 20 }; //1. 이거를
+function 함수2(_a) {
+    var student2 = _a.student2, age2 = _a.age2;
+    console.log(student2, age2); // ture, 20
+}
+함수2({ student2: true, age2: 20 }); // 2. 이렇게 아예 파리미터로 박어버림
+// 숙제 1 
+function max() {
+    var x = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        x[_i] = arguments[_i];
+    }
+    var result = 0; // 2. 비교할 변수생성
+    x.forEach(function (i) {
+        if (result < i) { // 4. 파라미터로 들어온 숫자를 계속 result와 비교
+            result = i; // 5. result가 i보다 작으면(즉, i가 더 크면) result에 i를 넣음 -> 그리고 다음 숫자와 result를 비교
+        } // 6. result가 i보다 작지 않으면!! 걍 냅둠 
+    });
+    return result; // 7. result return
+}
+console.log(max(1, 2, 3, 4, 5, 5));
+function 함수3(_a) {
+    var user = _a.user, comment = _a.comment, admin = _a.admin;
+    console.log(user, comment, admin);
+}
+함수3({ user: "kim", comment: [3, 5, 4], admin: false });
+// 숙제 3
+function 함수4(_a) {
+    var a = _a.slice(0);
+    console.log(a); // 여기도 a, b, c로 가능 & 위에 파라미터 설정해줄때는 대괄호 빼먹지 말기.. 
+}
+함수4([40, "wine", false]);
--- index.ts
+++ index.ts
@@ -1,35 +1,80 @@
-// Object 타입지정(interface) 연습해보기
+// 함수 rest parameter, destructuring할 때 타입지정
 
-// interface 
-interface Square {  // class 만드는 법이랑 유사함 
-  color : string,   // 콤마(,)말고 세미콜론(;) 가능
-  width : number
+// 1. rest parameter
+function 함수(...a :number[]){  // 점 3개 붙이면 rest parameter / 인입될 파라미터의 개수를 모를때 쓸 수 있음(다른 파라미터와 같이 쓸 때 맨 뒤에 써야 함)
+  console.log(a);
+}
+함수(1,2,3,4,5,6)
+
+// 1-2. ...spread operation
+// array나 object왼쪽에 쓰면 괄호를 벗겨달라는 문법임.
+let arr1 = [1,2,3];
+let arr2 = [4,5];
+let arr3 = [...arr1, ...arr2];  // console 출력결과 : 1,2,3,4,5
+console.log(arr3);
+
+
+
+// 2. destructuring 
+// 2-1. destructuring - arr
+let [변수1, 변수2] = ["안녕", 100];
+console.log(변수1);
+console.log(변수2);
+// 2-2.destructuring - obj
+let { student: student, age: age } = { student: true, age: 20 };  //변수명과 속성명을 맞추는게 편리(헷갈릴 수 있음)
+let { student1, age1 } = { student1: true, age1: 20 };  // 변수명과 속성명이 동일하면 생략이 가능함
+console.log(student1);
+console.log(age1);
+
+
+
+// 3. 함수에도 destructuring문법 사용 가능
+// ex. obj안에 있던 자료를 파라미터로 집어넣고 싶다면?
+let 오브젝트 = { student2: true, age2: 20 };  //1. 이거를
+
+type 함수2 = { 
+  student2: boolean;
+  age2: number;
 }
 
-let 네모 :Square = { color : 'red' , width : 100 }
-
-
-
-
-// interfaceextends문법 연습
-interface Student {
-  name: string;
+function 함수2({ student2, age2 }: 함수2) {  // 3. 그리고 함수에 destructuring문법 사용
+  console.log(student2, age2); // ture, 20
 }
-interface Teacher extends Student { // extends 문법 사용 가능
-  age: number;
+함수2({ student2: true, age2: 20 });  // 2. 이렇게 아예 파리미터로 박어버림
+
+
+
+// 숙제 1 
+function max(...x : number[]) :number{  // 1. number 타입의 rest parameter 생성
+  let result = 0; // 2. 비교할 변수생성
+  x.forEach((i) => {  // 3. 반복문 생성
+    if(result < i){   // 4. 파라미터로 들어온 숫자를 계속 result와 비교
+      result = i;   // 5. result가 i보다 작으면(즉, i가 더 크면) result에 i를 넣음 -> 그리고 다음 숫자와 result를 비교
+    } // 6. result가 i보다 작지 않으면!! 걍 냅둠 
+  })
+  return result;  // 7. result return
+}
+console.log(max(1,2,3,4,5,5));
+
+
+// 숙제 2
+type 함수3 = {
+  user: string;
+  comment: number[];
+  admin: boolean;
+};
+
+function 함수3({ user, comment, admin }: 함수3): void {
+  console.log(user, comment, admin);
 }
 
-let 학생: Student = { name: "kim" };
-let 선생: Teacher = { name: "kim", age: 20 };
+함수3({ user: "kim", comment: [3, 5, 4], admin: false });
 
 
-// &기호 (intersection type) : extends와 유사 
-type Animal = { name: string };
-type Cat = { age: number } & Animal;
 
-let 고양이: Cat = { name: "나비", age: 3 };
+// 숙제 3
+function 함수4([...a] : (number|string|boolean)[]){ // 파라미터를 [a, b, c]로 해줘도 가능
+  console.log(a);   // 여기도 a, b, c로 가능 & 위에 파라미터 설정해줄때는 대괄호 빼먹지 말기.. 
+}
 
-
-  // type VS interface
-  // - interface는 중복선언 가능
-  // - type은 중복선언 불가능
+함수4([40, "wine", false]);
