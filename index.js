aca62d3f00084c05d69604419c9758615765e48b
qwe8851 <dgh07027@naver.com>
2022-12-05 23:27:10 +0900

    class에서 사용가능한 protected, static 키워드

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
-// 함수 rest parameter, destructuring할 때 타입지정
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+// class에서 사용가능한 protected, static 키워드
+// - class를 복사해서 또다른 class를 만들고 싶을 때
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// 1. extends
+var User = /** @class */ (function () {
+    function User() {
+        this.x = 10;
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// 1. rest parameter
-function 함수() {
-    var a = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        a[_i] = arguments[_i];
+    return User;
+}());
+var NewUser = /** @class */ (function (_super) {
+    __extends(NewUser, _super);
+    function NewUser() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    console.log(a);
-}
-함수(1, 2, 3, 4, 5, 6);
-// 1-2. ...spread operation
-// array나 object왼쪽에 쓰면 괄호를 벗겨달라는 문법임.
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5];
-var arr3 = __spreadArray(__spreadArray([], arr1, true), arr2, true); // console 출력결과 : 1,2,3,4,5
-console.log(arr3);
-// 2. destructuring 
-// 2-1. destructuring - arr
-var _a = ["안녕", 100], 변수1 = _a[0], 변수2 = _a[1];
-console.log(변수1);
-console.log(변수2);
-// 2-2.destructuring - obj
-var _b = { student: true, age: 20 }, student = _b.student, age = _b.age; //변수명과 속성명을 맞추는게 편리(헷갈릴 수 있음)
-var _c = { student1: true, age1: 20 }, student1 = _c.student1, age1 = _c.age1; // 변수명과 속성명이 동일하면 생략이 가능함
-console.log(student1);
-console.log(age1);
-// 3. 함수에도 destructuring문법 사용 가능
-// ex. obj안에 있던 자료를 파라미터로 집어넣고 싶다면?
-var 오브젝트 = { student2: true, age2: 20 }; //1. 이거를
-function 함수2(_a) {
-    var student2 = _a.student2, age2 = _a.age2;
-    console.log(student2, age2); // ture, 20
-}
-함수2({ student2: true, age2: 20 }); // 2. 이렇게 아예 파리미터로 박어버림
-// 숙제 1 
-function max() {
-    var x = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        x[_i] = arguments[_i];
+    return NewUser;
+}(User));
+var 사람 = new NewUser();
+console.log(사람);
+// 2. protected
+// - class{}안에서만 사용 가능한 private키워드와 유사
+// - private보단 약간 더 확장성을 제공해줌
+var User2 = /** @class */ (function () {
+    function User2() {
+        this.x = 10; // newUser2에서 사용 불가
+        this.y = 10; // newUser2에서 사용 가능
     }
-    var result = 0; // 2. 비교할 변수생성
-    x.forEach(function (i) {
-        if (result < i) { // 4. 파라미터로 들어온 숫자를 계속 result와 비교
-            result = i; // 5. result가 i보다 작으면(즉, i가 더 크면) result에 i를 넣음 -> 그리고 다음 숫자와 result를 비교
-        } // 6. result가 i보다 작지 않으면!! 걍 냅둠 
-    });
-    return result; // 7. result return
-}
-console.log(max(1, 2, 3, 4, 5, 5));
-function 함수3(_a) {
-    var user = _a.user, comment = _a.comment, admin = _a.admin;
-    console.log(user, comment, admin);
-}
-함수3({ user: "kim", comment: [3, 5, 4], admin: false });
-// 숙제 3
-function 함수4(_a) {
-    var a = _a.slice(0);
-    console.log(a); // 여기도 a, b, c로 가능 & 위에 파라미터 설정해줄때는 대괄호 빼먹지 말기.. 
-}
-함수4([40, "wine", false]);
+    return User2;
+}());
+var NewUSer2 = /** @class */ (function (_super) {
+    __extends(NewUSer2, _super);
+    function NewUSer2() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    NewUSer2.prototype.doThis = function () {
+        this.x = 20; // 에러
+        this.y = 20; // 사용 가능
+    };
+    return NewUSer2;
+}(User2));
+// - protected : extends된 class는 사용 가능, 자식들 사용 불가능
+// - private : extends된 class는 사용 불가능, 자식들 사용 불가능
+// 3. static
+// - private/protected/public + static 가능!
+var User3 = /** @class */ (function () {
+    function User3() {
+        this.y = 20;
+    }
+    User3.x = 10; // static 키워드를 붙이면 부모 class에 직접 부여됨 (+ 자식에게 안물려줌)
+    return User3;
+}());
+var 자식 = new User3();
+console.log(자식); // static 키워드를 붙인 x는 나오지 않음.
+// 4. static 활용 예시
+var User4 = /** @class */ (function () {
+    function User4() {
+        this.intro = User4.skill + "전문가입니다."; // 그래서 부모class인 User4.skill이라고 써야 함 
+    }
+    User4.skill = "js"; // static으로 선언된 변수는 부모만 갖다 쓸 수 있음
+    return User4;
+}());
+var 철수 = new User4();
+console.log(철수); // js 전문가입니다.
+// skill을 js에서 ts로 변경
+User4.skill = 'ts';
+var 맹구 = new User4();
+console.log(맹구); // ts 전문가입니다.
+// - 하지만 값을 감추려는 의도라면 protected, private를 쓰는게 더 옳은 방법이라고 할 수 있음.
--- index.ts
+++ index.ts
@@ -1,69 +1,69 @@
-// public, private, protected, static 키워드
-// - 이런 객체 지향 언어같은 문법도 제공됨.
-// - class를 많이 만들어서 개발하면 유용함
+// class에서 사용가능한 protected, static 키워드
+// - class를 복사해서 또다른 class를 만들고 싶을 때
 
 
 
 
-
-// 1. public 키워드
-class User1 {
-  public name: string = 'kim';  // public이 붙으면 모든 자식들이 이용가능
-
-  constructor(a) {
-    this.name = a;
-  }
+// 1. extends
+class User {
+  x = 10;
 }
 
-let 유저1 = new User1('park'); // public은 안써도 default값으로 생략되어 있음.
-유저1.name = '안뇽';
+class NewUser extends User {
+  //User class에 있던 내용을 NewUser에 복붙해줌
+}
+
+let 사람 = new NewUser();
+console.log(사람);
 
 
 
 
-
-// 1. private 키워드
+// 2. protected
+// - class{}안에서만 사용 가능한 private키워드와 유사
+// - private보단 약간 더 확장성을 제공해줌
 class User2 {
-  name: string; // 범위 : class 중괄호{}
-  private familyName: string = "park";
-
-  constructor(a) {
-    this.name = a + this.familyName;
-  }
-
-  이름변경함수() {
-    this.familyName = "kim";
-  }
+  private x = 10;   // newUser2에서 사용 불가
+  protected y = 10; // newUser2에서 사용 가능
 }
 
-let 유저2 = new User2("민수");
-유저2.familyName = "안뇽";    //오류: private타입이라 수정권한이 없음.
-console.log(유저2);
-
-
-
-
-
-// 자식들이 familyName을 바꾸고 싶으면
-// 1. class안에 familyName 변경 함수 제작
-// 2. 자식들이 사용
-유저2.이름변경함수();
-console.log(유저2);
-
-// 데이터를 외부로부터 보호하고싶을 때 자주 사용하는 패턴임
-
-
-
-
-
-// +) public 키워드를 쓰면 this.어쩌구 생략 가능
-class Person { 
-
-  constructor(public name: string){   // "이 자리에 들어온 파라미터는 자식의 name속성에 기입"해달라는 뜻
-    
+class NewUSer2 extends User2{
+  doThis(){
+    this.x = 20;    // 에러
+    this.y = 20;    // 사용 가능
   }
+}
+// - protected : extends된 class는 사용 가능, 자식들 사용 불가능
+// - private : extends된 class는 사용 불가능, 자식들 사용 불가능
 
+
+
+
+// 3. static
+// - private/protected/public + static 가능!
+class User3 {
+  static x = 10;    // static 키워드를 붙이면 부모 class에 직접 부여됨 (+ 자식에게 안물려줌)
+  y = 20;
 }
 
-let 자식 = new Person('kim');
-console.log(자식);
+let 자식 = new User3();
+console.log(자식);    // static 키워드를 붙인 x는 나오지 않음.
+
+
+
+
+// 4. static 활용 예시
+class User4 {
+  static skill = "js";    // static으로 선언된 변수는 부모만 갖다 쓸 수 있음
+  intro = User4.skill + "전문가입니다.";    // 그래서 부모class인 User4.skill이라고 써야 함 
+}
+let 철수 = new User4();
+console.log(철수);    // js 전문가입니다.
+
+// skill을 js에서 ts로 변경
+User4.skill = 'ts';
+
+let 맹구 = new User4();
+console.log(맹구);    // ts 전문가입니다.
+
+// - 하지만 값을 감추려는 의도라면 protected, private를 쓰는게 더 옳은 방법이라고 할 수 있음.
