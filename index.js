94198beb7cf46f076502c3dae6ce867c9fdeb838
qwe8851 <dgh07027@naver.com>
2022-12-06 22:43:45 +0900

    private, static사용해서 class 생성 연습

--- index.js
+++ index.js
@@ -1,82 +1,62 @@
-// class에서 사용가능한 protected, static 키워드
-// - class를 복사해서 또다른 class를 만들고 싶을 때
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-// 1. extends
+// class 생성 숙제 
+// 숙제1) 다음 x, y속성의 특징을 설명해보십시오.
 var User = /** @class */ (function () {
     function User() {
-        this.x = 10;
     }
+    User.x = 10;
+    User.y = 10;
     return User;
 }());
-var NewUser = /** @class */ (function (_super) {
-    __extends(NewUser, _super);
-    function NewUser() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return NewUser;
-}(User));
-var 사람 = new NewUser();
-console.log(사람);
-// 2. protected
-// - class{}안에서만 사용 가능한 private키워드와 유사
-// - private보단 약간 더 확장성을 제공해줌
+// 정답
+// 1. 필드값은 원래 모든 USer자식들에게 물려주는 속성이지만 
+//    x와 y는 static 키워드가 붙었기 때문에 User.x 이런식으로 접근해서 사용할 수 있다.
+//    User 자식들은 x와 y를 쓸 수 없다.
+// 2. private static x는 class내부에서만 수정이 가능하고
+//    public static y는 class 내외부 상관없이 수정이가능하다.
+//    public 키워드를 지워도 동일하게 동작한다.
+// 3. protected  는 private 키워드와 유사하게 class 내부에서만 사용이 가능한데
+//    약간 범위가 넓어서 extends로 복사한 class내부에서도 사용이 가능하다.
+// 숙제2) x 속성에 숫자를 더해주는 함수가 필요합니다. 
 var User2 = /** @class */ (function () {
     function User2() {
-        this.x = 10; // newUser2에서 사용 불가
-        this.y = 10; // newUser2에서 사용 가능
     }
+    User2.addOne = function (p) {
+        (User2.x += p);
+    };
+    User2.printX = function () {
+        console.log(User2.x);
+    };
+    User2.x = 10;
+    User2.y = 20;
     return User2;
 }());
-var NewUSer2 = /** @class */ (function (_super) {
-    __extends(NewUSer2, _super);
-    function NewUSer2() {
-        return _super !== null && _super.apply(this, arguments) || this;
+User2.addOne(3);
+User2.addOne(4);
+User2.printX();
+// 숙제3) 네모.draw()를 할 때마다 index.html에 가로 30px 세로 30px, 배경색이 'red'의 <div>박스 무작위 배치
+var Square = /** @class */ (function () {
+    function Square(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
-    NewUSer2.prototype.doThis = function () {
-        this.x = 20; // 에러
-        this.y = 20; // 사용 가능
+    Square.prototype.draw = function () {
+        var random = Math.random();
+        var square = "<div style=\"position:relative;\n    top:".concat(random * 400, "px;\n    left:").concat(random * 400, "px;\n    width:").concat(this.width, "px;\n    height:").concat(this.height, "px;\n    background:").concat(this.color, "px\n    </div>");
+        document.body.insertAdjacentHTML('beforeend', square);
     };
-    return NewUSer2;
-}(User2));
-// - protected : extends된 class는 사용 가능, 자식들 사용 불가능
-// - private : extends된 class는 사용 불가능, 자식들 사용 불가능
-// 3. static
-// - private/protected/public + static 가능!
-var User3 = /** @class */ (function () {
-    function User3() {
-        this.y = 20;
-    }
-    User3.x = 10; // static 키워드를 붙이면 부모 class에 직접 부여됨 (+ 자식에게 안물려줌)
-    return User3;
+    return Square;
 }());
-var 자식 = new User3();
-console.log(자식); // static 키워드를 붙인 x는 나오지 않음.
-// 4. static 활용 예시
-var User4 = /** @class */ (function () {
-    function User4() {
-        this.intro = User4.skill + "전문가입니다."; // 그래서 부모class인 User4.skill이라고 써야 함 
-    }
-    User4.skill = "js"; // static으로 선언된 변수는 부모만 갖다 쓸 수 있음
-    return User4;
-}());
-var 철수 = new User4();
-console.log(철수); // js 전문가입니다.
-// skill을 js에서 ts로 변경
-User4.skill = 'ts';
-var 맹구 = new User4();
-console.log(맹구); // ts 전문가입니다.
-// - 하지만 값을 감추려는 의도라면 protected, private를 쓰는게 더 옳은 방법이라고 할 수 있음.
+var 네모 = new Square(30, 30, "red");
+네모.draw();
+네모.draw();
+네모.draw();
+네모.draw();
+// 1. constructor를 이용해서 새로 뽑는 obj들은 width, height, color를 입력할 수 있게 만들었 음.
+// 2. 자식들은 drow()를 쓰면
+// 2-1. 0과 1사이의 무작위 숫자를 뽑고, 그걸 변수 random에 저장함
+// 2-2. <div>를 디자인함. 근데 <div>박스의 폭, 높이, 색상은 constructor이 입력한 것들을 활용함
+// 2-3. <div>박스의 위치는 left, right속성을 이용해 0~400px사이로 무작위 배치함
+// 2-4. insertAdjacentHTML이런거 이용하면 원하는 곳에 html을 추가가 가능함
+// 그래서 실제로 자식을 하나 뽑아서 drow()했더니 진짜 빨간 박스 4개가 나옴.
+// 다른 사이즈, 다른 색상으로도 뽑아서 drow() 이것도 가능하겠군@!
--- index.ts
+++ index.ts
@@ -1,69 +1,74 @@
-// class에서 사용가능한 protected, static 키워드
-// - class를 복사해서 또다른 class를 만들고 싶을 때
+// class 생성 숙제 
 
-
-
-
-// 1. extends
+// 숙제1) 다음 x, y속성의 특징을 설명해보십시오.
 class User {
-  x = 10;
+  private static x = 10;
+  public static y = 10;
 }
+// 정답
+// 1. 필드값은 원래 모든 USer자식들에게 물려주는 속성이지만 
+//    x와 y는 static 키워드가 붙었기 때문에 User.x 이런식으로 접근해서 사용할 수 있다.
+//    User 자식들은 x와 y를 쓸 수 없다.
+// 2. private static x는 class내부에서만 수정이 가능하고
+//    public static y는 class 내외부 상관없이 수정이가능하다.
+//    public 키워드를 지워도 동일하게 동작한다.
+// 3. protected  는 private 키워드와 유사하게 class 내부에서만 사용이 가능한데
+//    약간 범위가 넓어서 extends로 복사한 class내부에서도 사용이 가능하다.
 
-class NewUser extends User {
-  //User class에 있던 내용을 NewUser에 복붙해줌
-}
-
-let 사람 = new NewUser();
-console.log(사람);
 
 
 
 
-// 2. protected
-// - class{}안에서만 사용 가능한 private키워드와 유사
-// - private보단 약간 더 확장성을 제공해줌
+// 숙제2) x 속성에 숫자를 더해주는 함수가 필요합니다. 
 class User2 {
-  private x = 10;   // newUser2에서 사용 불가
-  protected y = 10; // newUser2에서 사용 가능
-}
+  private static x = 10;
+  public static y = 20;
 
-class NewUSer2 extends User2{
-  doThis(){
-    this.x = 20;    // 에러
-    this.y = 20;    // 사용 가능
+  static addOne(p) {
+    (User2.x += p);
+  }
+
+  static printX(){
+    console.log(User2.x);
   }
 }
-// - protected : extends된 class는 사용 가능, 자식들 사용 불가능
-// - private : extends된 class는 사용 불가능, 자식들 사용 불가능
+User2.addOne(3);
+User2.addOne(4);
+User2.printX();
 
 
 
 
-// 3. static
-// - private/protected/public + static 가능!
-class User3 {
-  static x = 10;    // static 키워드를 붙이면 부모 class에 직접 부여됨 (+ 자식에게 안물려줌)
-  y = 20;
+
+
+// 숙제3) 네모.draw()를 할 때마다 index.html에 가로 30px 세로 30px, 배경색이 'red'의 <div>박스 무작위 배치
+class Square {
+  constructor(public width:number, public height:number, public color:string) {}
+
+  draw(){
+    let random = Math.random();
+    let square = `<div style="position:relative;
+    top:${random*400}px;
+    left:${random*400}px;
+    width:${this.width}px;
+    height:${this.height}px;
+    background:${this.color}px
+    </div>`;
+    document.body.insertAdjacentHTML('beforeend', square);
+  }
 }
 
-let 자식 = new User3();
-console.log(자식);    // static 키워드를 붙인 x는 나오지 않음.
+let 네모 = new Square(30, 30, "red");
+네모.draw();
+네모.draw();
+네모.draw();
+네모.draw();
+// 1. constructor를 이용해서 새로 뽑는 obj들은 width, height, color를 입력할 수 있게 만들었 음.
+// 2. 자식들은 drow()를 쓰면
+// 2-1. 0과 1사이의 무작위 숫자를 뽑고, 그걸 변수 random에 저장함
+// 2-2. <div>를 디자인함. 근데 <div>박스의 폭, 높이, 색상은 constructor이 입력한 것들을 활용함
+// 2-3. <div>박스의 위치는 left, right속성을 이용해 0~400px사이로 무작위 배치함
+// 2-4. insertAdjacentHTML이런거 이용하면 원하는 곳에 html을 추가가 가능함
 
-
-
-
-// 4. static 활용 예시
-class User4 {
-  static skill = "js";    // static으로 선언된 변수는 부모만 갖다 쓸 수 있음
-  intro = User4.skill + "전문가입니다.";    // 그래서 부모class인 User4.skill이라고 써야 함 
-}
-let 철수 = new User4();
-console.log(철수);    // js 전문가입니다.
-
-// skill을 js에서 ts로 변경
-User4.skill = 'ts';
-
-let 맹구 = new User4();
-console.log(맹구);    // ts 전문가입니다.
-
-// - 하지만 값을 감추려는 의도라면 protected, private를 쓰는게 더 옳은 방법이라고 할 수 있음.
+// 그래서 실제로 자식을 하나 뽑아서 drow()했더니 진짜 빨간 박스 4개가 나옴.
+// 다른 사이즈, 다른 색상으로도 뽑아서 drow() 이것도 가능하겠군@!
