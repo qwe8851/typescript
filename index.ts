73eb630e146741e604c4204c70806db0da91f07e
qwe8851 <dgh07027@naver.com>
2022-12-15 23:45:42 +0900

    외부 파일 이용시 declare & 이상한 특징인 ambient module

--- index.ts
+++ index.ts
@@ -1,95 +1,26 @@
-// array자료에 붙일 수 있는 tuple type
 
-// 🤔 [tuple type]
-// 위치까지 고려한 엄격한 타입지정 가능
-// - 첫 자료는 무조건 string
-// - 둘째 자료는 무조건 boolean
-
-let 멍멍: (string | boolean)[] = ["dog", true];
-// 👇
-let 멍멍2: [string, boolean] = ["dog", true];
+// 1. ts파일에서 js파일을 가져다 쓰고 싶으면 declare를 써주면 됨
+// (data.js
+declare let a : number;
+console.log(a + 1);
 
 
 
-
-
-// ✨ tuple 안에 옵션표시 가능
-let 멍멍option: [string, boolean?] = ["dog"];
-
-// 1. 순서 주의!
-let 멍멍option2: [string, boolean?, number] = ["dog", 3];
-// - option은 마지막에만 쓸 수 있음
-
-// - 두개이상도 가능!
-let 멍멍option3: [string, boolean?, number?] = ["dog", true];
+// 2. ts파일끼리의 변수사용은 import export
+import { b } from './data2';
+console.log(b + 1);
 
 
 
+// 📎 ts특 : 모든 ts파일은 ambient module(글로벌 모듈)
+// - 그래서 ts파일끼리는 import export없이도 다른 파일에 있던 변수들을 가져다 쓸 수 있음.
 
-// ✨ 함수에서 쓰는 tuple
-function 함수(...rest : [number, string]){
+// 📎 ts파일을 ambient모듈이 아니라 로컬 모듈로 만드는 법 
+// - ts파일 안에 import export가 있으면 자동으로 로컬모듈이 된다.
+export {} // 로컬모듈
+
+
+declare global{
+    // 로컬모듈에서 글로벌 변수를 만들때 사용 (타입/인터페이스 만들때 주로 활용)
+    type Dog = string;
 }
-함수(1, '2');
-// 그냥 일반 함수 파라미터 타입지정 VS rest parameter
-//  - rest파라미터를 사용한 함수는 파라미터가 array[]에 담겨져 온다는 차이가 있음.
-
-
-
-
-
-
-// ✨ ...spread operator
-let arr = [1, 2, 3];
-let arr2 = [4, 5, ...arr];  // 4, 5, 1, 2, 3 : 괄호를 벗겨줌
-
-// ...spread operator의 타입지정은?
-let arr3: [number, number, ...number[]] = [4, 5, ...arr];
-
-
-
-
-
-
-// 📝 숙제 1) 음식의 1.이름 2.가격 3.맛있는지 여부을 array 자료에 담아보고 타입지정
-let greenTea : [string, number, boolean] = ['동서녹차', 4000, true];
-
-// 응용) 
-type greenTeaType = [string, number, ...boolean[]]
-let greenTeaA: greenTeaType = ["동서녹차", 4000, true, false, true, true, false, true];
-
-
-
-// 📝 숙제 2) 함수 타입지정
-//  - 첫번째 파라미터는 문자, 
-//  - 두번째는 boolean,
-//  -  세번째는 숫자 또는 문자
-function hw2(...rest : [string, boolean, (string|number)[]]){
-
-}
-hw2("1", true, 2, "3", "4", 5, 6);
-
-
-
-// 📝 숙제 3) 문자/숫자 분류기 생성
-//  - 파라미터 중 문자와 숫자를 각각 모아 []에 담기
-//  - 문자 숫자 외에는 자료 입력 불가능 
-//  - 파라미터/ return 타입 지정
-
-function Classification(...rest: (string | number)[]) {
-    let result: [string[], number[]] = [[], []];
-
-    rest.forEach((a) => {
-        if (typeof a === "string") {
-            result[0].push(a);
-        } else {
-            result[1].push(a);
-        }
-    });
-
-    return result;  // return 타입 동일 
-}
-Classification("b", 5, 6, 8, "a");
-
-// 1. 함수 파라미터는 rest parameter로 여러개를 입력 받을 수 있음. 타입은 (string|number)[]
-// 2. 결과를 저장할 result변수 생성. 기본값은 [[],[]]. 타입은 tuple type 활용
-// 3. rest parameter 반복문 돌림.
